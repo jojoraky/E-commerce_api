239b07bdd51fdbe1f50c64c0d5fa803c3ca3f556
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-09-16 22:40:46 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
-
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+host: env('HOST', '0.0.0.0'),
+url: env('PUBLIC_URL', 'https://jhj-t2lu.onrender.com'),
+proxy: env.bool('IS_PROXIED', true),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://jhj-t2lu.onrender.com'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', false),
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
