a75fa02d3b7420783a2bf0082a3268a7cb8ba5d7
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-09-16 23:54:21 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   url: env('PUBLIC_URL', 'https://testingaa.onrender.com'),
   proxy: env.bool('IS_PROXIED', true),
-    port: env.int('PORT', 1337),
+    port: env.int('PORT', 80),
     app: {
       keys: env.array('APP_KEYS'),
     },
