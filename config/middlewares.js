64283508cc20c51a630cb8bc2ed6df97dccac500
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-09-16 23:36:17 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,16 +1,42 @@
-module.exports = {
-  load: {
-    before: ['timer', 'responseTime', 'logger', 'cors', 'responses', 'gzip'],
-    after: ['parser', 'router'],
-  },
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['*'], // Replace this with the allowed origins
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
+
+module.exports = [
+  // The array is pre-populated with internal, built-in middlewares, prefixed by `strapi::`
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+
+  // custom middleware that does not require any configuration
+  'global::my-custom-node-module', 
+
+  // custom name to find a package or a path
+  {
+    name: 'my-custom-node-module',
+    config: {
+      foo: 'bar',
     },
   },
-};
+
+  // custom resolve to find a package or a path
+  {
+    resolve: '../some-dir/custom-middleware',
+    config: {
+      foo: 'bar',
+    },
+  },
+
+  // custom configuration for internal middleware
+  {
+    name: 'strapi::poweredBy',
+    config: {
+      poweredBy: 'Some awesome company',
+    },
+  },
+
+  // remaining internal & built-in middlewares
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
