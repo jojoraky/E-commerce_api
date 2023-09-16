e9d3b7664e0e8a9ebaa8d056e802b864746325b6
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-09-16 22:34:17 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,9 @@
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  socket: '/tmp/nginx.socket', // only use if absolutely required
-  emitErrors: false,
   url: env('PUBLIC_URL', 'https://jhj-t2lu.onrender.com'),
   proxy: env.bool('IS_PROXIED', true),
   cron: {
